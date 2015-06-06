a3d0848106200059bff097fcc57926e3b848f7e2
Raphael Kargon <raphael.kargon@gmail.com>
2015-06-06 14:43:41 -0400

    Added musical scales

--- src/musicCode/MarkovNoteGenerator.java
+++ src/musicCode/MarkovNoteGenerator.java
@@ -6,15 +6,16 @@ import jm.music.data.Note;
 public class MarkovNoteGenerator implements JMC {
   Note currentNote;
 
-  // TODO params like tempo, amount of repetition, etc.
+  // TODO params like tempo, key, amount of repetition, etc.
   public MarkovNoteGenerator() {
-    currentNote = new Note(C4, QN);
+    currentNote = new Note(C4, QUARTER_NOTE, MEZZO_FORTE);
   }
 
   public Note nextNote() {
     int pitch = currentNote.getPitch();
     int newpitch = Util.clamp(pitch + (int) (Math.random() * 5) - 2, CN1, G9);
-    Note newNote = new Note(newpitch, QN);
+    Note newNote = currentNote.copy();
+    newNote.setPitch(newpitch);
     currentNote = newNote;
     return newNote.copy();
   }
