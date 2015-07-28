6678083e0c39dafa940cd6b135f444a05617f7d3
Raphael Kargon <raphael.kargon@gmail.com>
2015-07-28 00:29:20 -0400

    added changes to testtrackgenerator, now in markovgenerator nowh's testtrackgenerator.java code is unchanged.
    Changes (now in markovgenerator) include:
    Random function now evenly goes up and down, distinction is made between # of notes in scale and # of notes in octave ( the latter is always 12)
    Scales and Keys abstracted out as separate classes.

--- src/generators/MarkovGenerator.java
+++ src/generators/MarkovGenerator.java
@@ -1,49 +1,145 @@
 package generators;
 
-import instruments.Piano;
-import jm.music.data.Note;
-import jm.music.data.Part;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import jm.music.data.Phrase;
+import musicCode.Key;
 import musicCode.Track;
-import musicCode.Util;
+import scales.Scale;
 
-/**
- * Generates a series of notes through a markov chain. i.e. Keeps track of the
- * current note, and the next note is shifted a small, random amount up or down
- * from the current one.
- *
- * TODO add paramaters like tempo, key, amount of repetition, etc
- *
- * TODO vary note duration
- *
- * TODO keep track of bars, make sure note durations in each bar add up properly
- *
- * @author raphaelkargon
- *
- */
 public class MarkovGenerator implements TrackGenerator {
-  private Note currentNote;
-
-  public MarkovGenerator() {
-    currentNote = new Note(C4, QUARTER_NOTE, MEZZO_FORTE);
-  }
-
-  public Note nextNote() {
-    final int pitch = currentNote.getPitch();
-    final int newpitch =
-        Util.clamp(pitch + (int) (Math.random() * 5) - 2, CN1, G9);
-    final Note newNote = currentNote.copy();
-    newNote.setPitch(newpitch);
-    currentNote = newNote;
-    return newNote.copy();
-  }
 
   @Override
-  public Track generate(Track track) {
-    Track t = new Track("Markov Track", new Piano());
-    Part p = t.getMusic();
-    for (int i = 1; i <= 50; i++) {
-      p.addNote(nextNote(), i);
+  public Track generate(Track trackN) {
+    final Track track = trackN;
+    final Phrase ph = new Phrase();
+    track.add(ph);
+    ph.setTempo(140);
+
+    final Key key = getKey();
+
+    // int baseNote = 0;
+    // int baseOctave = 0;
+    // final int notesInOctave = 7;
+    // final int octaveCount = 12;
+
+    int currentOctave = 0, currentIndex = 0;
+    for (int i = 0; i < 2000; i++) {
+      // get index for base note of current chord, as well as the note's octave
+      int indexChange =
+          (int) (Math.random() * 5) - 2 + (int) (Math.random() * 5) - 2;
+      currentIndex += indexChange;
+      if (currentIndex < 0) {
+        currentOctave--;
+      } else if (currentIndex >= key.getNotes().length) {
+        currentOctave++;
+      }
+      currentIndex =
+          (currentIndex + key.getNotes().length) % key.getNotes().length;
+
+      List<Integer[]> chords = key.getChords();
+      Integer[] randChord = chords.get((int) (Math.random() * chords.size()));
+
+      final int[] outChord = new int[randChord.length];
+      for (int j = 0; j < outChord.length; j++) {
+        int newIndex = currentIndex + randChord[j];
+        int newOctave = currentOctave;
+        if (newIndex < 0) {
+          newOctave--;
+        } else if (newIndex >= key.getNotes().length) {
+          newOctave++;
+        }
+        newIndex = (newIndex + key.getNotes().length) % key.getNotes().length;
+        int chordNote = key.getNotes()[newIndex] + newOctave * 12;
+        outChord[j] = chordNote;
+      }
+
+      // random walk sometimes goes out of bounds, we need to fix this (and can
+      // do so rather easily)
+      boolean outOfBounds = false;
+      for (int note : outChord) {
+        if (note > 127 || note < 0) {
+          outOfBounds = true;
+          break;
+        }
+      }
+      if (outOfBounds) {
+        break;
+      }
+      ph.addChord(outChord, Math.random() * 0.1 + 1.95);
     }
-    return t;
+
+
+
+    /*
+     * final int[] chordB1 = { c3, e3, g3 }; final int[] chordB2 = { d3, f3, a3
+     * }; final int[] chordB3 = { d3, b2, g2 }; final int[] chordB4 = { c3, a2,
+     * f2 };
+     *
+     * for (int i = 0; i < 8; i++) {
+     *
+     * ph.addChord(chordB1, 2.0); ph.addChord(chordB2, 2.0);
+     * ph.addChord(chordB3, 2.0); ph.addChord(chordB4, 2.0);
+     *
+     * }
+     */
+
+    return track;
   }
+
+  public void printTonic(int n) {
+    System.out.println(((Integer) n).toString() + " = "
+        + (char) ((n - 46) % 7 + 65));
+  }
+
+  /**
+   * Generates a key based on a random base note, a scale of notes, and a set of
+   * chords.
+   *
+   * @return a Key
+   */
+  public Key getKey() {
+    final int baseNote = (int) (Math.random() * 11) + 48;
+    final int[] notePattern = {0, 2, 4, 5, 7, 9, 11};
+    // final int[] notePattern = { 0, 2, 3, 5, 7, 8, 10 };
+    Scale sc = new Scale(baseNote, notePattern);
+    final List<Integer[]> chords = getChords();
+
+    System.out.println(baseNote);
+    System.out.println(Arrays.toString(sc.getTones()));
+    System.out.println(chords);
+
+    return new Key(sc, chords);
+  }
+
+  public List<Integer[]> getChords() {
+    final List<Integer[]> chords = new ArrayList<>();
+
+    final Integer[] major = {0, 2, 4};
+    final Integer[] major5 = {0, 2};
+    final Integer[] power = {0, 4};
+    final Integer[] sus = {0, 3, 4};
+    final Integer[] sus2 = {0, 1, 4};
+    final Integer[] sixth = {0, 2, 4, 5};
+    final Integer[] sixth5 = {0, 2, 5};
+    final Integer[] sixth9 = {0, 2, 5, 8};
+    final Integer[] seventh = {0, 2, 4, 6};
+    final Integer[] seventh5 = {0, 2, 6};
+
+    chords.add(major);
+    chords.add(major5);
+    chords.add(power);
+    chords.add(sus);
+    chords.add(sus2);
+    chords.add(sixth);
+    chords.add(sixth5);
+    chords.add(sixth9);
+    chords.add(seventh);
+    chords.add(seventh5);
+
+    return chords;
+  }
+
 }
--- src/generators/TestTrackGenerator.java
+++ src/generators/TestTrackGenerator.java
@@ -1,21 +1,16 @@
 package generators;
 
-import static musicCode.Util.toIntArray;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import jm.JMC;
 import jm.music.data.Phrase;
-import musicCode.Key;
 import musicCode.Track;
-import scales.Scale;
 
 public class TestTrackGenerator implements TrackGenerator {
 
   @Override
   public Track generate(Track trackN) {
+
     final Track track = trackN;
     final Phrase ph = new Phrase();
     track.add(ph);
@@ -29,100 +24,51 @@ public class TestTrackGenerator implements TrackGenerator {
     final int notesInOctave = 7;
     final int octaveCount = 12;
 
-    int currentOctave = 0, currentIndex = 0;
-    boolean NoahsVersion = false;
-    if (!NoahsVersion) {
-      for (int i = 0; i < 20000; i++) {
-        int indexChange = (int) (Math.random() * 5) - 2;
-        currentIndex += indexChange;
-        System.out.println("unclamepd current index: " + currentIndex);
-        if (currentIndex < 0) {
-          currentOctave--;
-        } else if (currentIndex >= key.getNotes().length) {
-          currentOctave++;
-        }
-        currentIndex =
-            (currentIndex + key.getNotes().length) % key.getNotes().length;
-        int currentNote = key.getNotes()[currentIndex] + currentOctave * 12;
+    for (int i = 0; i < 20000; i++) {
+      final int newValue =
+          baseNote + baseOctave * notesInOctave + (int) (Math.random() * 4) - 2
+              + (int) (Math.random() * 4) - 2;
+      baseNote = (newValue + notesInOctave) % notesInOctave;
+      baseOctave = newValue / notesInOctave;
 
+      System.out.println("Next:" + baseNote);
+      System.out.println(baseOctave);
 
-        System.out.println("Current index:" + currentIndex);
-        System.out.println("Current octave: " + currentOctave);
+      final List<Integer[]> chords = key.getChords();
+      final Integer[] relChord =
+          chords.get((int) (Math.random() * (chords.size() - 1)));
 
-        List<Integer[]> chords = key.getChords();
-        Integer[] randChord = chords.get((int) (Math.random() * chords.size()));
+      System.out.println(relChord);
 
+      final List<Integer> chord = new ArrayList<>();
+      for (final int relNote : relChord) {
 
-        System.out.println(randChord);
+        int varNote = baseNote + relNote;
+        final int varOctave = varNote / notesInOctave;
+        varNote = varNote % notesInOctave;
 
-        final int[] outChord = new int[randChord.length];
-        for (int j = 0; j < outChord.length; j++) {
-          int chordNote = currentNote + randChord[j];
-          outChord[j] = chordNote;
-        }
-
-        System.out.println(Arrays.toString(outChord));
-        boolean outOfBounds = false;
-        for (int note : outChord) {
-          if (note > 127 || note < 0) {
-            outOfBounds = true;
-            break;
-          }
-        }
-        if (outOfBounds) {
-          break;
-        }
-        ph.addChord(outChord, Math.random() * 0.1 + 1.95);
-      }
-    } else {
-
-      for (int i = 0; i < 2000; i++) {
-        final int newValue =
-            baseNote + baseOctave * notesInOctave + (int) (Math.random() * 4)
-                - 2 + (int) (Math.random() * 4) - 2;
-        baseNote = (newValue + notesInOctave) % notesInOctave;
-        baseOctave = newValue / notesInOctave;
-
-        System.out.println("Next:" + baseNote);
-        System.out.println(baseOctave);
-
-        final List<Integer[]> chords = key.getChords();
-        final Integer[] relChord =
-            chords.get((int) (Math.random() * (chords.size() - 1)));
-
-        System.out.println(relChord);
-
-        final List<Integer> chord = new ArrayList<>();
-        for (final int relNote : relChord) {
-
-          int varNote = baseNote + relNote;
-          final int varOctave = varNote / notesInOctave;
-          varNote = varNote % notesInOctave;
-
-          chord.add(key.getNotes()[varNote] + (baseOctave + varOctave)
-              * octaveCount);
-        }
-
-        System.out.println(chord);
-
-        printTonic(key.getBaseNote());
-        System.out.println(c3);
-
-        ph.addChord(toIntArray(chord), Math.random() * 0.1 + 1.95);
+        chord.add(key.getNotes().get(varNote) + (baseOctave + varOctave)
+            * octaveCount);
       }
 
+      System.out.println(chord);
+
+      printTonic(key.getBaseNote());
+      System.out.println(c3);
+
+      ph.addChord(toIntArray(chord), Math.random() * 0.1 + 1.95);
     }
 
     /*
      * final int[] chordB1 = { c3, e3, g3 }; final int[] chordB2 = { d3, f3, a3
      * }; final int[] chordB3 = { d3, b2, g2 }; final int[] chordB4 = { c3, a2,
      * f2 };
-     *
+     * 
      * for (int i = 0; i < 8; i++) {
-     *
+     * 
      * ph.addChord(chordB1, 2.0); ph.addChord(chordB2, 2.0);
      * ph.addChord(chordB3, 2.0); ph.addChord(chordB4, 2.0);
-     *
+     * 
      * }
      */
 
@@ -134,24 +80,29 @@ public class TestTrackGenerator implements TrackGenerator {
         + (char) ((n - 46) % 7 + 65));
   }
 
-  /**
-   * Generates a key based on a random base note, a scale of notes, and a set of
-   * chords.
-   *
-   * @return a Key
-   */
+  int[] toIntArray(List<Integer> list) {
+    final int[] ret = new int[list.size()];
+    for (int i = 0; i < ret.length; i++)
+      ret[i] = list.get(i);
+    return ret;
+  }
+
   public Key getKey() {
     final int baseNote = (int) (Math.random() * 11) + 48;
+    System.out.println(baseNote);
+
     final int[] notePattern = {0, 2, 4, 5, 7, 9, 11};
     // final int[] notePattern = { 0, 2, 3, 5, 7, 8, 10 };
-    Scale sc = new Scale(baseNote, notePattern);
-    final List<Integer[]> chords = getChords();
+    final List<Integer> notes = new ArrayList<>();
+    for (final int note : notePattern) {
+      notes.add(baseNote + note);
+    }
+    System.out.println(notes);
 
-    System.out.println(baseNote);
-    System.out.println(Arrays.toString(sc.getTones()));
+    final List<Integer[]> chords = getChords();
     System.out.println(chords);
 
-    return new Key(sc, chords);
+    return new Key(notes, baseNote, chords);
   }
 
   public List<Integer[]> getChords() {
@@ -180,6 +131,31 @@ public class TestTrackGenerator implements TrackGenerator {
     chords.add(seventh5);
 
     return chords;
+  }
+
+  public class Key {
+    private final List<Integer> notes;
+    private final int baseNote;
+    private final List<Integer[]> chords;
+
+    public Key(List<Integer> notesN, int baseNoteN, List<Integer[]> chordsN) {
+      this.notes = notesN;
+      this.baseNote = baseNoteN;
+      this.chords = chordsN;
+    }
+
+    public List<Integer> getNotes() {
+      return notes;
+    }
+
+    public int getBaseNote() {
+      return baseNote;
+    }
+
+    public List<Integer[]> getChords() {
+      return chords;
+    }
+
   }
 
 }
